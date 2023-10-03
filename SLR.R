0ff7d6554ab81cab8ecf2983e60b3c1a3e62f842
Aakash Karthikeyan <aakashkarthikeyan3@gmail.com>
2023-10-03 14:11:07 +0530

    R file final

--- SLR.R
+++ SLR.R
@@ -12,3 +12,45 @@ test_set = subset(dataset,split==FALSE)
 
 regressor = lm(formula=AvgTestScores ~ YearsEducation,
 data=training_set)
+
+#predicting the test set results
+
+y_pred = predict(regressor,newdata= test_set)
+
+# Visualising model performance on training set
+
+library(ggplot2)
+
+ggplot() +
+
+geom_point(aes(x=training_set$YearsEducation,
+y=training_set$AvgTestScores),
+colour = 'red') +
+
+geom_line(aes(x=training_set$YearsEducation,
+y = predict(regressor,newdata= training_set)),
+colour = 'blue') +
+
+ggtitle('Education vs Test scores (Training set)')+
+xlab('Years of Education') +
+ylab('Test Scores')
+
+
+# Visualising model performance on t set
+
+library(ggplot2)
+
+ggplot() +
+
+geom_point(aes(x=test_set$YearsEducation,
+y=test_set$AvgTestScores),
+colour = 'red') +
+
+geom_line(aes(x=test_set$YearsEducation,
+y = predict(regressor,newdata= test_set)),
+colour = 'blue') +
+
+ggtitle('Education vs Test scores (Training set)')+
+xlab('Years of Education') +
+ylab('Test Scores')
+

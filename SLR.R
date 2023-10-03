e83ff0b32f88b21636fa728ba93ab5905831db94
Aakash Karthikeyan <aakashkarthikeyan3@gmail.com>
2023-10-03 14:15:38 +0530

    R file

--- SLR.R
+++ SLR.R
@@ -1,56 +1,43 @@
+# Load the necessary libraries
+library(caTools)
+library(ggplot2)
+
+# Read the dataset
 dataset = read.csv('score.csv')
 
-library(caTools)
+# Set the seed for reproducibility
 set.seed(123)
-split = sample.split(dataset$AvgTestScores, SplitRatio=2/3)
-training_set = subset(dataset,split==TRUE)
-test_set = subset(dataset,split==FALSE)
 
+# Split the dataset into training and test sets
+split = sample.split(dataset$AvgTestScores, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
 # SIMPLE LINEAR REGRESSION #
 
+regressor = lm(formula = AvgTestScores ~ YearsEducation, data = training_set)
 
-regressor = lm(formula=AvgTestScores ~ YearsEducation,
-data=training_set)
-
-#predicting the test set results
-
-y_pred = predict(regressor,newdata= test_set)
+# Predicting the test set results
+y_pred = predict(regressor, newdata = test_set)
 
 # Visualising model performance on training set
+training_plot <- ggplot() +
+  geom_point(aes(x = training_set$YearsEducation, y = training_set$AvgTestScores), colour = 'red') +
+  geom_line(aes(x = training_set$YearsEducation, y = predict(regressor, newdata = training_set)), colour = 'blue') +
+  ggtitle('Education vs Test scores (Training set)') +
+  xlab('Years of Education') +
+  ylab('Test Scores')
 
-library(ggplot2)
+# Save the training plot as a PNG file
+ggsave("training_plot.png", plot = training_plot)
 
-ggplot() +
+# Visualising model performance on test set
+test_plot <- ggplot() +
+  geom_point(aes(x = test_set$YearsEducation, y = test_set$AvgTestScores), colour = 'red') +
+  geom_line(aes(x = test_set$YearsEducation, y = predict(regressor, newdata = test_set)), colour = 'blue') +
+  ggtitle('Education vs Test scores (Test set)') +
+  xlab('Years of Education') +
+  ylab('Test Scores')
 
-geom_point(aes(x=training_set$YearsEducation,
-y=training_set$AvgTestScores),
-colour = 'red') +
-
-geom_line(aes(x=training_set$YearsEducation,
-y = predict(regressor,newdata= training_set)),
-colour = 'blue') +
-
-ggtitle('Education vs Test scores (Training set)')+
-xlab('Years of Education') +
-ylab('Test Scores')
-
-
-# Visualising model performance on t set
-
-library(ggplot2)
-
-ggplot() +
-
-geom_point(aes(x=test_set$YearsEducation,
-y=test_set$AvgTestScores),
-colour = 'red') +
-
-geom_line(aes(x=test_set$YearsEducation,
-y = predict(regressor,newdata= test_set)),
-colour = 'blue') +
-
-ggtitle('Education vs Test scores (Training set)')+
-xlab('Years of Education') +
-ylab('Test Scores')
-
+# Save the test plot as a PNG file
+ggsave("test_plot.png", plot = test_plot)
